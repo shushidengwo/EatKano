c8cb54611dc3d75e0577a518429277c1e5b6fca9
shushidengwo <130460522+shushidengwo@users.noreply.github.com>
2024-03-20 10:33:36 +0800

    Add files via upload

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 // Change this to your own configuration
-$link = new mysqli('localhost','NAME','PASSWORD','kano');
+$link = new mysqli('154.9.254.215','kano','SJa5688j7Ny4Rd4B','kano');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
     die("Failed to connect: " . $conn->connect_error);
